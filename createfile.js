a5bd7045ab2ad03b506caee247524a070a7d89f3
sethpaulproductions <seth@sethpaulproductions.com>
2015-08-22 09:46:00 -0700

    Update createfile.js

--- createfile.js
+++ createfile.js
@@ -9,7 +9,7 @@
        */
       function setProperties() {
         var bookingFolderID;
-        var artistName = "The Monday After";
+        var artistName = document.getElementById('idArtist').value;
         switch (artistName) {
         case "The Monday After":
             bookingFolderID = "0B3kQ1Mt6SSkhflBsQWVfbGtnalc5amhONjZRUnFiRkhUN2dNZ1VOTjFNcGtDenpvdmdjTEE";
