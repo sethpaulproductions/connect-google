82faef9afd73f2a09a2b880da2c5a5a2cdc628b5
sethpaulproductions <seth@sethpaulproductions.com>
2015-08-22 12:00:46 -0700

    Update createfile.js

--- createfile.js
+++ createfile.js
@@ -98,17 +98,15 @@
             });
         }
         
-        var initialRequest = gapi.client.drive.children.list({
-           'folderId' : '0B3kQ1Mt6SSkhflBsQWVfbGtnalc5amhONjZRUnFiRkhUN2dNZ1VOTjFNcGtDenpvdmdjTEE'
-        });
 
-        /*var initialRequest = gapi.client.request({
+
+        var initialRequest = gapi.client.request({
             'path': '/drive/v2/files/0B3kQ1Mt6SSkhflBsQWVfbGtnalc5amhONjZRUnFiRkhUN2dNZ1VOTjFNcGtDenpvdmdjTEE/children',
             'method': 'GET',
             'params': {
             'q': 'trashed = false and mimeType = "application/vnd.google-apps.folder"'
             }
-        });*/
+        });
         retrievePageOfFiles(initialRequest, []);
       } 
        
